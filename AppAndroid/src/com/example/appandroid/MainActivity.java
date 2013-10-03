d65684370a7db3cbce00fe4fc3ca8f9b13e0d116
LuizFilipe <luiz.moresco@hotmail.com>
2013-10-03 15:15:13 -0300

    dsa

--- AppAndroid/src/com/example/appandroid/MainActivity.java
+++ AppAndroid/src/com/example/appandroid/MainActivity.java
@@ -1,23 +1,41 @@
 package com.example.appandroid;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	private Teste meuTeste = new Teste();
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+		Button bt = (Button) findViewById(R.id.btEntrar);
+		
+		bt.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				EditText meuEditi = (EditText) findViewById(R.id.etSenha);
+				meuTeste.setA(meuEditi.getText().toString());
+				TextView tvTitle = (TextView) findViewById( R.id.tvTitle );
+				tvTitle.setText( meuTeste.getA() );
+			}
+		});
+	}
 
+	/*
+	 * @Override public boolean onCreateOptionsMenu(Menu menu) { // Inflate the
+	 * menu; this adds items to the action bar if it is present.
+	 * getMenuInflater().inflate(R.menu.main, menu); return true; }
+	 */
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
 }
---
+++ AppAndroid/src/com/example/appandroid/Teste.java
@@ -0,0 +1,14 @@
+package com.example.appandroid;
+
+public class Teste
+{
+	private String a = "dsadas";
+
+	public String getA() {
+		return a;
+	}
+
+	public void setA(String a) {
+		this.a = a;
+	}
+}
