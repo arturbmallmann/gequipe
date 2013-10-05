2dfbd0c47c33fecb6f3da2b9125ab9422cb98d99
LuizFilipe <luiz.moresco@hotmail.com>
2013-10-05 16:03:54 -0300

    SEMPRE MANDAR ESSES ARQUIVOS - IMPORTANTE

--- AppAndroid/gen/com/example/appandroid/R.java
+++ AppAndroid/gen/com/example/appandroid/R.java
@@ -24,27 +24,37 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int btEntrar=0x7f080005;
-        public static final int editText2=0x7f080001;
-        public static final int etSenha=0x7f080002;
-        public static final int textView1=0x7f080003;
-        public static final int textView3=0x7f080004;
-        public static final int tvTitle=0x7f080000;
+        public static final int Titulo=0x7f080000;
+        public static final int action_settings=0x7f080008;
+        public static final int btAdicionarMembroEquipe=0x7f080003;
+        public static final int btAdicionarProjeto=0x7f080006;
+        public static final int btAdicionarTarefa=0x7f080002;
+        public static final int btAtualizarTarefa=0x7f080005;
+        public static final int btRemoverProjeto=0x7f080004;
+        public static final int btVisualizarEquipe=0x7f080007;
+        public static final int tvTitulo=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int adicionar_tarefa=0x7f030000;
+        public static final int adicionar_tarefaa=0x7f030001;
+        public static final int principal_activity=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int adicionar_tarefa=0x7f070000;
+        public static final int adicionar_tarefa_activity=0x7f070001;
+        public static final int main=0x7f070002;
+        public static final int minha=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int entrar=0x7f050005;
+        public static final int hello_world=0x7f050007;
         public static final int login=0x7f050003;
         public static final int senha=0x7f050004;
         public static final int texto=0x7f050002;
+        public static final int title_activity_adicionar_tarefa=0x7f050008;
+        public static final int title_activity_adicionar_tarefa_activity=0x7f050006;
     }
     public static final class style {
         /** 
